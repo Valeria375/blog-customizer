2d5f7caba36c23598335420500d379322f25534a
Валерия <146752405+Valeria375@users.noreply.github.com>
2024-06-27 22:41:47 +0300

    все работает

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -14,21 +14,23 @@ import {
 	OptionType,
 	backgroundColors,
 	contentWidthArr,
+	defaultArticleState,
 	fontColors,
+	fontFamilyClasses,
 	fontFamilyOptions,
 	fontSizeOptions,
 } from 'src/constants/articleProps';
 
 type ArticleParamsFormProps = {
 	currentSettings: ArticleStateType;
-	applySettings?: (newState: ArticleStateType) => void;
+	applySettings: (newState: ArticleStateType) => void;
 };
 
 export const ArticleParamsForm = ({
 	currentSettings,
 	applySettings,
 }: ArticleParamsFormProps) => {
-	const [isMenuOpen, setIsMenuOpen] = useState(true);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const rootRef = useRef<HTMLDivElement>(null);
 	const toggleForm = () => {
 		// alert('клик на стрелку')
@@ -39,44 +41,62 @@ export const ArticleParamsForm = ({
 
 	const handleSubmit = (event: React.FormEvent) => {
 		event.preventDefault();
-		// applySettings(formState);
+		applySettings(formState);
+		// console.log(formState)
 	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		applySettings(defaultArticleState);
+	  };
+	  const handleOptionChange = (optionType: keyof ArticleStateType) => (selected: OptionType) => {
+		setFormState((prevState) => ({
+		  ...prevState,
+		  [optionType]: selected,
+		}));
+	  };
+
 	return (
 		<div ref={rootRef}>
 			<ArrowButton onClick={toggleForm} isOpen={isMenuOpen} />
 			<aside
 				className={clsx(styles.container, isMenuOpen && styles.container_open)}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={handleSubmit}>
 					<Text>Задайте параметры</Text>
 					<Select
 						title='Шрифт'
 						options={fontFamilyOptions}
 						selected={formState.fontFamilyOption}
+						onChange={handleOptionChange('fontFamilyOption')}
 					/>
 					<RadioGroup
+					title='Размер шрифта'
 						name='Размер шрифта'
 						options={fontSizeOptions}
 						selected={formState.fontSizeOption}
-						title='Размер шрифта'
+						onChange={handleOptionChange('fontSizeOption')}
 					/>
 					<Select
 						title='Цвет шрифта'
 						options={fontColors}
 						selected={formState.fontColor}
+						onChange={handleOptionChange('fontColor')}
 					/>
 					  <Separator />
 					<Select
 						title='Цвет фона'
 						options={backgroundColors}
 						selected={formState.backgroundColor}
+						onChange={handleOptionChange('backgroundColor')}
 					/>
 					<Select
 						title='Ширина контента'
 						options={contentWidthArr}
 						selected={formState.contentWidth}
+						onChange={handleOptionChange('contentWidth')}
 					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={handleReset}/>
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
